61c79386ce670a7fef4512e7b3bf652c7aaa6951
mohd. siddique <siddiqmohd2772@gmail.com>
2024-06-22 21:51:02 +0530

    packages

---
+++ practice_set12.java
@@ -0,0 +1,17 @@
+package com.pk;
+class calculator{
+    calculator(){
+        System.out.println("in calculator");
+    }
+}
+class sccalculator{
+    sccalculator(){
+        System.out.println("in sccalculator");
+    }
+}
+class HybridCalculator{
+    HybridCalculator(){
+        System.out.println("in HybridCalculator");
+    }
+}
+
