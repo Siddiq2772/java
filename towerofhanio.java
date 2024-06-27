116b077c885c8bf20730d4485d201a63f11dfcaf
mohd. siddique <siddiqmohd2772@gmail.com>
2024-06-27 10:53:56 +0530

    tower of hanio project

---
+++ towerofhanio.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class towerofhanio {
+int arr[][]= new int[3][3],top[]=new int[3],vpop;
+
+towerofhanio(){
+    for (int i = 2; i >= 0; i--) {
+        arr[0][i]=i+1;}
+        top[0]=2;
+}
+public void print(){
+    for (int i = 2; i >= 0; i--) {
+        System.out.print("  ["+arr[0][i]+"]  ["+arr[1][i]+"]  ["+arr[2][i]+"]");
+        System.out.println();
+    }
+}
+public void pop(int a){
+    a-=1;
+  vpop=arr[a][top[a]];
+  top[a]--;
+}
+public void push(int a){
+    a-=1;
+    arr[a][top[a]]=vpop;
+    top[a]++;
+  }
+public static void main(String[] args) {
+    towerofhanio t = new towerofhanio();
+    while (true) {
+        t.print();
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter to pop form: ");
+        t.pop(sc.nextInt());
+        System.out.print("Enter to push to: ");
+        t.push(sc.nextInt());
+        
+        sc.close();
+    }
+}
+    
+}
